f306cd4c7e1d387f697797b10f91a2875740d7e2
Мухаметшин Ильдан Ильнурович <mukhametshin.ii@phystech.edu>
2024-09-28 11:20:46 +0300

    Add files via upload

--- 1ая неделя/main1.py/z6.py
+++ 1ая неделя/main1.py/z6.py
@@ -1,11 +1,37 @@
 f= open('input3.txt', 'r')
-strnum = f.readline().strip()
+t = open('output3.txt', 'w')
+strnum = f.readline().split()
 op = f.readline().strip()
 b = f.readline().strip()
-dn = 0
-vol = 0
-d_l = []
-num = [int(i) for i in strnum.split()]
-print(b)
-a = int(num, b)
-print(a)
+b = int(b)
+for i in range(len(strnum)):  #перевод в 10чную СС при помощи int
+        strnum[i] = int(strnum[i], b)
+oper=1
+sm=0
+x_s = []
+if  op == '*':
+    for i in strnum:
+        oper *= i
+    while oper:
+        num = oper % b
+        x_s += str(num)
+        oper = oper // b
+    t.write(''.join(x_s[::-1]))
+if op == '-':
+    for i in strnum:
+        sm -= i
+    while sm:
+        num = sm % b
+        x_s += str(num)
+        sm = sm // b
+    t.write(''.join(x_s[::-1]))
+if op  == '+':
+    for i in strnum:
+        sm += i
+    while sm:
+        num = sm % b
+        x_s += str(num)
+        sm = sm // b
+    t.write(''.join(x_s[::-1]))
+f.close()
+t.close()
