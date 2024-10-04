d330818998dd905298755bbed856fb17a937f346
Мухаметшин Ильдан Ильнурович <mukhametshin.ii@phystech.edu>
2024-10-04 21:36:51 +0300

    Add files via upload

---
+++ 3 неделя/треугольник.py
@@ -0,0 +1,15 @@
+def paint_triangle(st, char):
+    if st % 2 == 0:                         
+        i = 1
+        for i in range(1,st//2 + 1):
+            print(char * i)
+        for i in range(st//2, 0, -1):
+            print(char * i)
+    else:
+        for i in range(1,int(st/2 + 2)):
+            print(char * i)
+        for i in range(int(st/2), 0, -1):
+            print(char * i)
+            
+paint_triangle(99, "{}")
+#взял у Динара
